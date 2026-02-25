c5b449613aaa8843ab3988f464d1e7bd5bf95b9a
@dants <122106257+iurizin09@users.noreply.github.com>
2026-02-25 09:27:27 -0300

    Add files via upload

---
+++ Aula1.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+/*Variaveis*/
+
+int main(void){
+
+
+    cout << "Ola Mundo " << endl;
+
+    system("pause");
+
+    return 0 ;
+}
